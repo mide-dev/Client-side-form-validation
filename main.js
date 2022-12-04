c1f1b35f3f50dffad326fb56250dbc597fbe7bc8
midedev <ibrahimayomide330@gmail.com>
2022-12-04 11:52:56 +0100

    feat:built first and last name validation

--- main.js
+++ main.js
@@ -18,51 +18,113 @@ const phoneNumberError = document.getElementById("err-msg-tel");
 const passwordError = document.getElementById("err-msg-password");
 const submitError = document.getElementById("err-msg-submit");
 
-// validate form parameters
-const validateForm = (e) => {
-  e.preventDefault();
+//  submit btn error check
+const submitErrorMsg = (err) => {
+  if (err.textContent !== "") {
+    submitError.innerHTML = "please fix the error(s) above";
+  } else {
+    submitError.innerHTML = "";
+  }
+};
 
-  //  global submit error
-  const submitErrorMsg = (err) => {
-    if (err.textContent !== "") {
-      submitError.innerHTML = "please fix the error(s) above";
-    } else {
-      submitError.innerHTML = "";
-    }
-  };
+// // dectect no inputs or empty space only in input
+const noInputsOrEmptySpacePresent = (input) => {
+  if (input.value.length === 0 || input.value.match(/^(\s)+$/)) {
+    return true;
+  }
+};
 
-  // FUNCTION TO VALIDATE NAME
-  const NameValidate = (input, errMessage) => {
-    // check for no input or when user inputs space only
-    //   prettier-ignore
-    if (input.value.length === 0 || input.value.match(/^(\s)+$/)) {
-      errMessage.innerHTML = "Name is required";
-      submitErrorMsg(errMessage);
-      return;
-      // Prevent special characters in name
-      //   prettier-ignore
-    } else if (input.value.match(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/)) {
-      errMessage.innerHTML = "special charaters are not allowed in name";
-      submitErrorMsg(errMessage);
-      return;
-      // Prevent numbers in name
-    } else if (input.value.match(/\d/)) {
-      errMessage.innerHTML = "numbers are not allowed in name";
-      submitErrorMsg(errMessage);
-      return;
-    } else {
-      // delete error msg if input is valid
-      errMessage.innerHTML = "";
-      submitErrorMsg(errMessage);
-    }
-  };
+// dectect special characters in input
+const SpecialCharactersPresent = (input) => {
+  if (input.value.match(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/)) {
+    return true;
+  }
+};
 
-  // call the function to validate first and last name inputs
-  NameValidate(firstNameInput, firstNameError);
-  NameValidate(lastNameInput, lastNameError);
+// detect numbers in input
+const NumbersPresent = (input) => {
+  if (input.value.match(/\d/)) {
+    return true;
+  }
+};
 
-  // LOGIC TO VALIDATE EMAIL
+// dectect if email is in correct format
+const emailCorrect = (input) => {
+  if (input.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    return true;
+  }
+};
+
+// VALIDATE NAME WHEN USER TYPING
+const nameValidate = (input, errMessage) => {
+  // check for no input or empty space only
+  if (noInputsOrEmptySpacePresent(input)) {
+    input.setAttribute("data-input", "not-valid");
+    return;
+
+    // prevent special characters in name
+  } else if (SpecialCharactersPresent(input)) {
+    errMessage.innerHTML = "special charaters are not allowed in name";
+    input.setAttribute("data-input", "not-valid");
+    return;
+
+    // Prevent numbers in name
+  } else if (NumbersPresent(input)) {
+    errMessage.innerHTML = "numbers are not allowed in name";
+    input.setAttribute("data-input", "not-valid");
+    return;
+  } else {
+    // delete error msg if input is valid
+    errMessage.innerHTML = "";
+    input.removeAttribute("data-input");
+    submitErrorMsg(errMessage);
+  }
+};
+
+// VALIDATE EMAIL WHEN USER TYPING
+const emailValidate = (input, errMessage) => {
+  if (!emailCorrect(input)) {
+    errMessage.innerHTML = "email is not valid";
+    input.setAttribute("data-input", "not-valid");
+    return;
+  } else {
+    // delete error msg if input is valid
+    errMessage.innerHTML = "";
+    input.removeAttribute("data-input");
+    submitErrorMsg(errMessage);
+  }
+};
+
+// Additional validation when submit button is clicked
+const submitValidate = (input, errMessage) => {
+  // name validation
+  if (noInputsOrEmptySpacePresent(input)) {
+    errMessage.innerHTML = "Name is required";
+    submitErrorMsg(errMessage);
+  } else if (SpecialCharactersPresent(input) || NumbersPresent(input)) {
+    submitErrorMsg(errMessage);
+  } else {
+    submitErrorMsg(errMessage);
+  }
 };
 
 // bind event listner
-contactFormSubmit.addEventListener("click", validateForm);
+firstNameInput.addEventListener("keyup", () => {
+  nameValidate(firstNameInput, firstNameError);
+});
+
+lastNameInput.addEventListener("keyup", () => {
+  nameValidate(lastNameInput, lastNameError);
+});
+
+emailInput.addEventListener("keyup", () => {
+  emailValidate(emailInput, emailError);
+});
+
+// event listner for submit button
+contactFormSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  submitValidate(firstNameInput, firstNameError);
+  submitValidate(lastNameInput, lastNameError);
+  emailValidate(emailInput, emailError);
+});
